f4bb1c3ae2ae7581eb5fc799e26d8358a894599a
Ankit Vashisth <dryisholeup@gmail.com>
2024-05-27 16:31:58 +0530

    done

--- new.cpp
+++ new.cpp
@@ -1,26 +1,72 @@
-#include <bits/stdc++.h>
+// #include <bits/stdc++.h>
+#include <iostream>
 using namespace std;
 
 int main(){
+    // int rowcount,colcount;
+    // cin>>rowcount;
+    // cin>>colcount;
+   
+    //hollow rectangle
 
-    
-    for(int row =0; row<3;row++){
-        if( row==0||row==2){
-            for(int col =0; col<5;col++){
-                cout<<"*";
-            }
+    // for(int row =0; row<rowcount;row++){
+    //     if( row==0||row==rowcount-1){
+    //         for(int col =0; col<colcount;col++){
+    //             cout<<"*";
+    //         }
         
-        }
-        else{
-            cout<<"*";
+    //     }
+    //     else{
+    //         cout<<"*";
 
-            for(int i=0; i<3;i++){
-                cout<<" ";
-            }
-            cout<<"*";
-        }
-        cout<<"\n";
-    }
+    //         for(int i=0; i<colcount-2;i++){
+    //             cout<<" ";
+    //         }
+    //         cout<<"*";
+    //     }
+    //     cout<<"\n";
+    // }
+
+    // for(int row =rowcount; row>0; row--){
+    //     for(int col =row; col>0; col--){
+    //         cout<<"* ";
+
+    // }
+    // cout<<"\n";
+    // }
+
+    // int n;
+    // cin>>n;
+
+    // for(int row=0; row<n; row++){
+
+    //     for(int col=0; col<n-row;col++){
+    //         cout<<col+1<<" ";
+    //     }
+    //     cout<<"\n";
+    // }
+
+    // for(int row=0; row<n;row++){
+
+    //     for(int col=0; col<n-row;col++){
+    //         cout<<col+1<<" ";
+    //     }
+    //     cout<<"\n";
+    // }
+
+    // for(int row=0; row<n; row++){
+
+        
+    //     for( int i=0; i<row; i++){
+
+    //         cout<<" ";
+    //     }
+        
+
+       
+    // }
+
 
     
+    return 0;
 }
