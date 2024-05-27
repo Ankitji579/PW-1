ed26e84d29165deb96ec7e9b4dde005c2da82155
Ankit Vashisth <dryisholeup@gmail.com>
2024-05-27 19:32:56 +0530

    both pyramid

--- new.cpp
+++ new.cpp
@@ -68,32 +68,62 @@ int main(){
 
 
 
-//Problem -2
+//Problem -1
 
-  int n;
-  cin>>n;
+//   int n;
+//   cin>>n;
   
-  for(int i=0; i<n;i++){
-  int k =0;
-    for(int j=0; j<((2*n)-1);j++){
+//   for(int i=0; i<n;i++){
+//   int k =0;
+//     for(int j=0; j<((2*n)-1);j++){
       
-        if(j<(n-i-1)){
-        cout<<" ";
+//         if(j<(n-i-1)){
+//         cout<<" ";
+//         }
+//           else if(k<(2*i+1)){
+//             cout<<"*";
+//             k++;
+//           }
+//         else{
+//           cout<<" ";
+//         }
+
+//     }
+      
+  
+//     cout<<"\n";
+// }
+  
+
+
+//Problem -2 (Half-Pyramid)
+
+int n;
+cin>>n;
+
+for(int row=0; row<n;row++){
+    int k=0;
+    for(int col=0; col<(2*n-1);col++){
+        if(col<(n-row-1)){
+            cout<<" ";
         }
-          else if(k<(2*i+1)){
-            cout<<"*";
+        else if( k<(2*row+1)){
+
+            if(k==0||k==2*row|| row==n-1){
+                cout<<"*"; 
+            }
+            else
+            cout<<" ";
             k++;
-          }
-        else{
-          cout<<" ";
         }
-
+        else{
+            cout<<" ";
+        }
     }
-      
-  
     cout<<"\n";
 }
-  
+
+
 
     
     return 0;
