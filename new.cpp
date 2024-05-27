5f359023a9571556ee7a9172276c17b326b61718
Ankit Vashisth <dryisholeup@gmail.com>
2024-05-27 18:43:44 +0530

    problem-2

--- new.cpp
+++ new.cpp
@@ -67,6 +67,34 @@ int main(){
     // }
 
 
+
+//Problem -2
+
+  int n;
+  cin>>n;
+  
+  for(int i=0; i<n;i++){
+  int k =0;
+    for(int j=0; j<((2*n)-1);j++){
+      
+        if(j<(n-i-1)){
+        cout<<" ";
+        }
+          else if(k<(2*i+1)){
+            cout<<"*";
+            k++;
+          }
+        else{
+          cout<<" ";
+        }
+
+    }
+      
+  
+    cout<<"\n";
+}
+  
+
     
     return 0;
 }
